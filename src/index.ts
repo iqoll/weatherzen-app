009cd5be2c106df298a091eb48e9ebeed59033db
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-10-18 10:34:12 +0700

    initial webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import openWeather from './assets/openweather.png'
+import './styles/index.css'
+console.log(123)
+
+const openWeatherImg = document.getElementById('openWeatherImg') as HTMLImageElement
+openWeatherImg.src= openWeather
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    publicPath: '',
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  devServer: {
+    watchFiles: ["src/**/*"],
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'WeatherZen',
+      filename: 'index.html',
+      template: 'src/template.html'
+    })
+  ]
+} 
