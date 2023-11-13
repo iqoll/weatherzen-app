0269f325b101f397a5cc86c068712a4705e5b8b2
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-11-13 08:28:43 +0700

    dotenv

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const config = require("./webpack.config");
+const Dotenv = require("dotenv-webpack")
 
 module.exports = {
   ...config,
   mode: "production",
+  plugins: [
+    new Dotenv()
+  ]
 };
