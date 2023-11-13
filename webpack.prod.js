86ba1d7427d149e479f7e4362b86099a2f8c1a0c
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-11-13 08:50:24 +0700

    dotenv on prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,12 @@
 const { merge} = require('webpack-merge')
-const configJs = require('./webpack.config') 
+const configJs = require('./webpack.config')
+const Dotenv = require('dotenv-webpack')
 
 const prodJs = {
   mode: "production",
+  plugins: [
+    new Dotenv()
+  ]
 }
 
 module.exports = merge(configJs, prodJs)
